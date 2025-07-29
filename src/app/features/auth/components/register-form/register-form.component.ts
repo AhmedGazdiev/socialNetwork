038873416504c38e8deaf3be6170455ddccce237
AhmedGazdiev <gazahmed907@gmail.com>
2025-07-29 17:54:20 +0300

    feat(register): валидация полей

--- src/app/features/auth/components/register-form/register-form.component.ts
+++ src/app/features/auth/components/register-form/register-form.component.ts
@@ -1,6 +1,18 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { passwordValidator } from '@features/auth/password.validator';
 import { TuiButton, TuiIcon, TuiTextfield } from '@taiga-ui/core';
 import { GoogleButtonComponent } from '../google-button/google-button.component';
 
@@ -13,6 +25,8 @@ import { GoogleButtonComponent } from '../google-button/google-button.component'
     GoogleButtonComponent,
     RouterLink,
     ReactiveFormsModule,
+    TuiIcon,
+    TuiTextfield,
   ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss',
@@ -20,28 +34,37 @@ import { GoogleButtonComponent } from '../google-button/google-button.component'
 })
 export class RegisterFormComponent {
   private readonly fb = inject(FormBuilder);
+  public readonly gender = ['male', 'female'];
 
-  public readonly registerForm = this.fb.group({
-    fullname: this.fb.control('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-    username: this.fb.control('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-    email: this.fb.control('', [Validators.required, Validators.email]),
-    password: this.fb.control('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-    confirmPassword: this.fb.control('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-  });
+  public registerForm = new FormGroup(
+    {
+      fullname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl('', Validators.required),
+    },
+    { validators: passwordValidator }
+  );
 
-  onSubmit() {
+  public fullname = computed(() => this.registerForm.get(['fullname']));
+  public username = computed(() => this.registerForm.get(['username']));
+  public email = computed(() => this.registerForm.get(['email']));
+  public password = computed(() => this.registerForm.get(['password']));
+  public confirmPassword = computed(() =>
+    this.registerForm.get(['confirmPassword'])
+  );
+
+  public onSubmit() {
     if (this.registerForm.valid) {
       console.log(this.registerForm.value);
     }
